3e1bbe3202053b3c31abd2ba1eed4d661f12470f
JimboAvocado <54549763+JimboAvocado@users.noreply.github.com>
2020-01-21 11:04:51 -0800

    Add files via upload

---
+++ stranger things.rb
@@ -0,0 +1,20 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 180
+live_loop:Strange do
+  play:C4
+  sleep 0.5
+  play:E4
+  sleep 0.5
+  play:G4
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:C5
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:G4
+  sleep 0.5
+  play:E4
+  sleep 0.5
+end
